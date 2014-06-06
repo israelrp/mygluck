c85bf469b39b42c04f7e101163670d38f455ac9a
isaelromero <israelromerop@me.com>
2014-06-06 18:30:36 -0500

    Usuario

---
+++ MyGluck.BI/Tools/Encryptor.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.Globalization;
+using System.IO;
+
+namespace MyGluck.BI.Tools
+{
+    public static class Encryptor
+    {
+        const int SaltValueSize = 4;
+        static readonly string PasswordHash = "N0v@teK.-2113!";
+        static readonly string VIKey = "@1B2c3D4e5F6g7H8";
+
+        public static string GenerataSaltValue()
+        {
+            string result = "";
+            UnicodeEncoding utf16 = new UnicodeEncoding();
+            if (utf16 != null)
+            {
+                Random random=new Random(unchecked((int)DateTime.Now.Ticks));
+                if (random != null)
+                {
+                    byte[] saltValue = new byte[SaltValueSize];
+                    random.NextBytes(saltValue);
+                    result = utf16.GetString(saltValue);
+                }
+            }
+            return result;
+        }
+
+        public static string HashPassword(string clearData, string saltValue, HashAlgorithm hash)
+        {
+            UnicodeEncoding encoding = new UnicodeEncoding();
+            string result = "";
+
+            if (clearData != null && hash != null && encoding != null)
+            {
+                // If the salt string is null or the length is invalid then
+                // create a new valid salt value.
+
+                if (saltValue == null)
+                {
+                    // Generate a salt string.
+                    saltValue = GenerataSaltValue();
+                }
+
+                // Convert the salt string and the password string to a single
+                // array of bytes. Note that the password string is Unicode and
+                // therefore may or may not have a zero in every other byte.
+
+                byte[] binarySaltValue = new byte[SaltValueSize];
+
+                binarySaltValue[0] = byte.Parse(saltValue.Substring(0, 2), System.Globalization.NumberStyles.HexNumber, CultureInfo.InvariantCulture.NumberFormat);
+                binarySaltValue[1] = byte.Parse(saltValue.Substring(2, 2), System.Globalization.NumberStyles.HexNumber, CultureInfo.InvariantCulture.NumberFormat);
+                binarySaltValue[2] = byte.Parse(saltValue.Substring(4, 2), System.Globalization.NumberStyles.HexNumber, CultureInfo.InvariantCulture.NumberFormat);
+                binarySaltValue[3] = byte.Parse(saltValue.Substring(6, 2), System.Globalization.NumberStyles.HexNumber, CultureInfo.InvariantCulture.NumberFormat);
+
+                byte[] valueToHash = new byte[SaltValueSize + encoding.GetByteCount(clearData)];
+                byte[] binaryPassword = encoding.GetBytes(clearData);
+
+                // Copy the salt value and the password to the hash buffer.
+
+                binarySaltValue.CopyTo(valueToHash, 0);
+                binaryPassword.CopyTo(valueToHash, SaltValueSize);
+
+                byte[] hashValue = hash.ComputeHash(valueToHash);
+
+                // The hashed password is the salt plus the hash value (as a string).
+
+                string hashedPassword = saltValue;
+
+                foreach (byte hexdigit in hashValue)
+                {
+                    result += hexdigit.ToString("X2", CultureInfo.InvariantCulture.NumberFormat);
+                }
+            }
+
+            return result;
+        }
+
+
+        public static string Encrypt(string plainText, string saltValue)
+        {
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+
+            byte[] keyBytes = new Rfc2898DeriveBytes(PasswordHash, Encoding.ASCII.GetBytes(saltValue)).GetBytes(256 / 8);
+            var symmetricKey = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.Zeros };
+            var encryptor = symmetricKey.CreateEncryptor(keyBytes, Encoding.ASCII.GetBytes(VIKey));
+
+            byte[] cipherTextBytes;
+
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                {
+                    cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                    cryptoStream.FlushFinalBlock();
+                    cipherTextBytes = memoryStream.ToArray();
+                    cryptoStream.Close();
+                }
+                memoryStream.Close();
+            }
+            return Convert.ToBase64String(cipherTextBytes);
+        }
+
+        public static string Decrypt(string encryptedText, string saltValue)
+        {
+            byte[] cipherTextBytes = Convert.FromBase64String(encryptedText);
+            byte[] keyBytes = new Rfc2898DeriveBytes(PasswordHash, Encoding.ASCII.GetBytes(saltValue)).GetBytes(256 / 8);
+            var symmetricKey = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.None };
+
+            var decryptor = symmetricKey.CreateDecryptor(keyBytes, Encoding.ASCII.GetBytes(VIKey));
+            var memoryStream = new MemoryStream(cipherTextBytes);
+            var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+            byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+
+            int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+            memoryStream.Close();
+            cryptoStream.Close();
+            return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount).TrimEnd("\0".ToCharArray());
+        }
+    }
+}
