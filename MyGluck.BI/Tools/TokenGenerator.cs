2d163b41617b2e08edb83f869d8d85052d7a9ea0
israelrp <israel@novatek.me>
2014-06-11 00:12:18 -0500

    Tools

---
+++ MyGluck.BI/Tools/TokenGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGluck.BI.Tools
+{
+    public static class TokenGenerator
+    {
+        public static string GenerateTokenTime()
+        {
+            string result = "";
+            byte[] time = BitConverter.GetBytes(DateTime.UtcNow.ToBinary());
+            byte[] key = new Guid().ToByteArray();
+            result = Convert.ToBase64String(time.Concat(key).ToArray());
+            return result;
+        }
+
+        public static bool IsValidTokenTime(string token)
+        {
+            bool result = false;
+            byte[] data = Convert.FromBase64String(token);
+            DateTime creationDate = DateTime.FromBinary(BitConverter.ToInt64(data, 0));
+            if (creationDate > DateTime.UtcNow.AddHours(24))
+            {
+                result = true;
+            }
+            return result;
+        }
+    }
+}
---
+++ MyGluck.BI/UserManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyGluck.Domain;
+using MyGluck.Dal.Implementation;
+
+namespace MyGluck.BI
+{
+    public static class UserManager
+    {
+        public bool IsValidUser(string UserName, string password)
+        {
+            bool result = false;
+
+            return result;
+        }
+
+        public UserProfile CreateUser(UserProfile userProfile, string password)
+        {
+            UserProfile result = null;
+            if (userProfile != null)
+            {
+                UserProfileDaoImpl userProfileDaoImpl = new UserProfileDaoImpl();
+                userProfile=userProfileDaoImpl.Insert(userProfile);
+                if (userProfileDaoImpl != null)
+                {
+                    webpages_Membership membership = new webpages_Membership();
+                    membership.CreateDate = DateTime.Now;
+                    membership.IsConfirmed = false;
+                    membership.UserId = userProfile.UserId;
+                    membership.PasswordSalt = Tools.Encryptor.GenerataSaltValue();
+                    membership.Password=Tools.Encryptor.HashPassword(membership.Password,membership.PasswordSalt,System.Security.Cryptography.MD5.Create());
+                    membership.PasswordFailuresSinceLastSuccess = 0;
+                    string tokenconfirmation = Tools.TokenGenerator.GenerateTokenTime();
+                    membership.ConfirmationToken = Tools.Encryptor.Encrypt(tokenconfirmation, membership.PasswordSalt);
+                    webpages_MembershipDaoImpl membershipDaoImpl = new webpages_MembershipDaoImpl();
+                    if (membershipDaoImpl.Insert(membership) != null)
+                    {
+                        //Todo correcto se devuelve true
+                        result = userProfile;
+                    }
+                    else
+                    {
+                        //Error se deshace la operación del UserProfile Insert
+                        userProfileDaoImpl.Delete(userProfile);
+                    }
+                }
+
+            }
+            return result;
+        }
+
+        public bool CreateOAuth(webpages_OAuthMembership oAuthMemberShip)
+        {
+            bool result = false;
+            oAuthMemberShip = new webpages_OAuthMembershipDaoImpl().Insert(oAuthMemberShip);
+            if (oAuthMemberShip != null)
+            {
+                result = true;
+            }
+            return result;
+        }
+
+        public bool AsingUserToRole(webpages_UsersInRole userInRole)
+        {
+            bool result = false;
+            userInRole = new webpages_UsersInRoleDaoImpl().Insert(userInRole);
+            if (userInRole != null)
+            {
+                result = true;
+            }
+            return result;
+        }
+    }
+}
